9a9d0d105c877e01ce637e9f0e489a2126eb7880
jeffrohlman <jcrp9f@mail.missouri.edu>
2018-09-06 22:38:14 -0500

    v2

--- utility/utility.js
+++ utility/utility.js
@@ -1,9 +1,13 @@
 window.onload = function(){
 	var sidebartext = "if you see this javscript sucks";
-	$.ajax({ url: "utility/mainSideBar.txt", success: function(file_content) {
-		console.log("loaded");
-		sidebartext = file_content;
-	}});
+	$.ajax({ url: "utility/mainSideBar.txt"})
+		.done(function(file_content) {
+			console.log("loaded");
+			sidebartext = file_content;
+		})
+		.fail(function(){
+			console.log("fail");
+		});
 	var sidebar = document.getElementById("sidebar");
 	sidebar.innerHTML = sidebartext;
 }
