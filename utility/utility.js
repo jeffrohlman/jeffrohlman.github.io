f7e5d76ede40d68dbad05fedb97161717ba7b2c1
jeffrohlman <jcrp9f@mail.missouri.edu>
2018-09-06 22:34:47 -0500

    Updated sidebar v1

--- utility/utility.js
+++ utility/utility.js
@@ -1,6 +1,7 @@
 window.onload = function(){
 	var sidebartext = "if you see this javscript sucks";
 	$.ajax({ url: "utility/mainSideBar.txt", success: function(file_content) {
+		console.log("loaded");
 		sidebartext = file_content;
 	}});
 	var sidebar = document.getElementById("sidebar");
