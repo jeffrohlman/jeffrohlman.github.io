4cb3f24a4abc79f5ae573891452fd35fdca0b726
jeffrohlman <jcrp9f@mail.missouri.edu>
2018-09-12 20:27:49 -0500

    Updated injection function

--- utility/utility.js
+++ utility/utility.js
@@ -1,14 +1,29 @@
 window.onload = function(){
-	var sidebartext = "if you see this javscript sucks";
 	$.ajax({ url: "utility/mainSideBar.txt"})
 		.done(function(file_content) {
-			console.log("loaded");
-			sidebartext = file_content;
-			sidebar.innerHTML = sidebartext;
+			var loc = window.location.pathname;
+			var dir = loc.substring(loc.lastIndexOf('.com') + 4);
+			if(dir.includes("/projects")){
+				injection(file_content, "Projects");
+			}
+			else if(dir.includes("/journal")){
+				injection(file_content, "Journal");
+			}
+			else{
+				injection(file_content, "Home");
+			}
 		})
 		.fail(function(){
-			console.log("fail");
+			document.getElementById("sidebar").innerHTML = "failed to load sidebar data";
 		});
-	var sidebar = document.getElementById("sidebar");
-	
+}
+
+//Project folder injection
+function injection(file, str){
+	var sidebar = file.substring(file.indexOf("#" + str) + 3 + str.length);
+	var loc1 = sidebar.indexOf("#");
+	document.getElementById("sidebar").innerHTML = sidebar.substring(0, loc1 - 2);
+	var header = sidebar.substring(loc1 + 3)
+	var loc2 = header.indexOf("#");
+	document.getElementById("header").innerHTML = header.substring(0, loc2 - 2);
 }
